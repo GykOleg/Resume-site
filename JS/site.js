3f5f722b9d9a999dc404fe211380a7fb2021a6cc
Dartww <dart65668@gmail.com>
2022-07-05 10:32:19 +0300

    Phone adaptive

--- JS/site.js
+++ JS/site.js
@@ -8,14 +8,22 @@ const techC = bodyElement.querySelector(".tech__cover");
 
 let showBage = () => bageObject.classList.add('open');
 let closeBage = () => bageObject.classList.remove('open');
-let openSkillBox = () => skillsC.classList.add('open-box');
-let closeSkillBox = () => skillsC.classList.remove('open-box');
-let openTechBox = () => techC.classList.add('open-box');
-let closeTechBox = () => techC.classList.remove('open-box');
+let SkillBox = () => {
+   if (skillsC.classList.contains('open-box')) {
+      skillsC.classList.remove('open-box');
+   } else {
+      skillsC.classList.add('open-box')
+   }
+};
+let TechBox = () => {
+   if (techC.classList.contains('open-box')) {
+      techC.classList.remove('open-box');
+   } else {
+      techC.classList.add('open-box')
+   }
+};
 
 buttonOpen.addEventListener("click", showBage);
 bageObject.addEventListener("click", closeBage);
-skills.addEventListener("mouseover", openSkillBox);
-skills.addEventListener("mouseout", closeSkillBox);
-tech.addEventListener("mouseover", openTechBox);
-tech.addEventListener("mouseout", closeTechBox);
+skills.addEventListener("click", SkillBox);
+tech.addEventListener("click", TechBox);
