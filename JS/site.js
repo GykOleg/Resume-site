efc2467e5af1b7300f93c402d3a8b06b375e8be6
Dartww <dart65668@gmail.com>
2022-07-05 12:14:23 +0300

    Adaptiver phone ver2

--- JS/site.js
+++ JS/site.js
@@ -1,29 +1,21 @@
 const bodyElement = document.body;
 const bageObject = bodyElement.querySelector(".bage__body");
 const buttonOpen = bodyElement.querySelector(".suit__bage");
-const skills = bodyElement.querySelector(".skills");
-const tech = bodyElement.querySelector(".tech");
-const skillsC = bodyElement.querySelector(".skills__cover");
-const techC = bodyElement.querySelector(".tech__cover");
+const box = bodyElement.querySelector(".box__body");
+const cover = bodyElement.querySelector(".box__cover");
 
 let showBage = () => bageObject.classList.add('open');
 let closeBage = () => bageObject.classList.remove('open');
-let SkillBox = () => {
-   if (skillsC.classList.contains('open-box')) {
-      skillsC.classList.remove('open-box');
+let OpenCloseBox = () => {
+   if (cover.classList.contains('open-cover')) {
+      cover.classList.remove('open-cover');
+      box.classList.remove('open-box-text');
    } else {
-      skillsC.classList.add('open-box')
-   }
-};
-let TechBox = () => {
-   if (techC.classList.contains('open-box')) {
-      techC.classList.remove('open-box');
-   } else {
-      techC.classList.add('open-box')
+      cover.classList.add('open-cover');
+      box.classList.add('open-box-text');
    }
 };
 
 buttonOpen.addEventListener("click", showBage);
 bageObject.addEventListener("click", closeBage);
-skills.addEventListener("click", SkillBox);
-tech.addEventListener("click", TechBox);
+box.addEventListener("click", OpenCloseBox);
