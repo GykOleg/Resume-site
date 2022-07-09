eae62bb7b4bbb8d1cdc5402a2cd4ecd84cf386b4
GykOleg <gygyryk@gmail.com>
2022-07-09 17:51:42 +0300

    Add jquery

--- JS/site.js
+++ JS/site.js
@@ -1,21 +1,23 @@
-const bodyElement = document.body;
-const bageObject = bodyElement.querySelector(".bage__body");
-const buttonOpen = bodyElement.querySelector(".suit__bage");
-const box = bodyElement.querySelector(".box__body");
-const cover = bodyElement.querySelector(".box__cover");
+$(function () {
+   const bodyElement = document.body;
+   const bageObject = bodyElement.querySelector(".bage__body");
+   const buttonOpen = bodyElement.querySelector(".suit__bage");
+   const box = bodyElement.querySelector(".box__body");
+   const cover = bodyElement.querySelector(".box__cover");
 
-let showBage = () => bageObject.classList.add('open');
-let closeBage = () => bageObject.classList.remove('open');
-let OpenCloseBox = () => {
-   if (cover.classList.contains('open-cover')) {
-      cover.classList.remove('open-cover');
-      box.classList.remove('open-box-text');
-   } else {
-      cover.classList.add('open-cover');
-      box.classList.add('open-box-text');
-   }
-};
+   let showBage = () => bageObject.classList.add('open');
+   let closeBage = () => bageObject.classList.remove('open');
+   let OpenCloseBox = () => {
+      if (cover.classList.contains('open-cover')) {
+         cover.classList.remove('open-cover');
+         box.classList.remove('open-box-text');
+      } else {
+         cover.classList.add('open-cover');
+         box.classList.add('open-box-text');
+      }
+   };
 
-buttonOpen.addEventListener("click", showBage);
-bageObject.addEventListener("click", closeBage);
-box.addEventListener("click", OpenCloseBox);
+   buttonOpen.addEventListener("click", showBage);
+   bageObject.addEventListener("click", closeBage);
+   box.addEventListener("click", OpenCloseBox);
+});
